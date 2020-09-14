f4285701d78b685fede953d9ad37abfcab855160
TNTobsidian <65720409+TNTobsidian@users.noreply.github.com>
2020-09-14 16:13:33 +0800

    aaa
    aaa

---
+++ a.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+c=int(input())
+a3 = a / 3
+b = b + a3
+c = c + a3
+b3 = b / 3
+a = a + b3
+c = c + b3
+c3 = c / 3
+a = a + c3
+b = b + c3
+print(a + ' ' + b + ' ' + c)
